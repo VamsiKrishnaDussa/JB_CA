e8fffb2e88521d8ca37ef960e9d707b72651f2db
RevanthDigitalInterakt <revanth@rezultize.com>
2025-03-11 11:49:02 +0530

    url change

--- activity/app/app.js
+++ activity/app/app.js
@@ -1,6 +1,6 @@
     //const express = require('express');
     const axios = require('axios');
-    const configJSON = require('../config/config-json');
+    const configJSON = require('../config/config-json.js');
     require("dotenv").config();
 
     module.exports = function inboundCallActivity(app, options) {
