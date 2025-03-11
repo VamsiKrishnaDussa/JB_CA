430442a3ff383dcacac51a91fb939b69973a4378
RevanthDigitalInterakt <revanth@rezultize.com>
2025-03-11 18:40:44 +0530

    mobile number removed and load

--- activity/src/index.js
+++ activity/src/index.js
@@ -1,19 +1,3 @@
-// define(["postmonger"], function (Postmonger) {
-//     var connection = new Postmonger.Session();
-//     var activity = {};
-
-//     connection.on("initActivity", function (data) {
-//         activity = data;
-//         console.log("SFMC Activity Initialized", data);
-//         connection.trigger("ready");
-//     });
-
-//     connection.on("requestedSave", function () {
-//         activity.arguments.execute.inArguments = [{ "mobileNumber": "{{Contact.Attribute.MobileNumber}}" }];
-//         connection.trigger("updateActivity", activity);
-//     });
-// });
-
 define(["postmonger"], function (Postmonger) {
     var connection = new Postmonger.Session();
     var activity = {};
@@ -25,8 +9,38 @@ define(["postmonger"], function (Postmonger) {
     });
 
     connection.on("requestedSave", function () {
-        activity.arguments.execute.inArguments = [{ "mobileNumber": "{{Contact.Attribute.MobileNumber}}" }];
+        if (!activity.arguments) {
+            activity.arguments = {};
+        }
+
+        var mobileNumber = $("#mobileNumber").val();
+
+        if (!mobileNumber || !/^\d{10}$/.test(mobileNumber)) {
+            alert("Please enter a valid 10-digit mobile number.");
+            return;
+        }
+
+        activity.arguments.execute = {
+            inArguments: [{ "mobileNumber": mobileNumber }],
+            outArguments: []
+        };
+
+        console.log("Saving activity:", activity);
         connection.trigger("updateActivity", activity);
     });
-});
 
+    connection.on("requestedValidate", function () {
+        var isValid = true;
+        var mobileNumber = $("#mobileNumber").val();
+
+        if (!mobileNumber || !/^\d{10}$/.test(mobileNumber)) {
+            isValid = false;
+            alert("Invalid mobile number. Please enter a 10-digit number.");
+        }
+
+        console.log("Validation result:", isValid);
+        connection.trigger("validateActivity", isValid);
+    });
+
+    connection.trigger("ready");
+});
