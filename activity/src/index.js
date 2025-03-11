75c243eef19ea2f4e3792925763f0269bb601b4a
RevanthDigitalInterakt <revanth@rezultize.com>
2025-03-11 15:34:11 +0530

    html changed

--- activity/src/index.js
+++ activity/src/index.js
@@ -4,54 +4,32 @@ require(['postmonger', 'jquery'], function (Postmonger, $) {
 
     $(document).ready(function () {
         connection.on('initActivity', onInitActivity);
-        connection.on('clickedNext', onDoneButtonClick);
-        connection.on('clickedBack', onBackButtonClick);
-        connection.on('clickedCancel', onCancelButtonClick);
         connection.trigger('ready');
     });
 
-    function onInitActivity(payload) {
-        console.log('in activity');
-        connection.trigger('ready');
-        // activity = payload;
-        // var hasInArguments = activity.arguments &&
-        //     activity.arguments.execute &&
-        //     activity.arguments.execute.inArguments &&
-        //     activity.arguments.execute.inArguments.length > 0;
+    // function onInitActivity(payload) {
+    //     console.log('in activity');
+    //     connection.trigger('ready');
+    //     // activity = payload;
+    //     // var hasInArguments = activity.arguments &&
+    //     //     activity.arguments.execute &&
+    //     //     activity.arguments.execute.inArguments &&
+    //     //     activity.arguments.execute.inArguments.length > 0;
 
-        // var inArguments = hasInArguments ? activity.arguments.execute.inArguments : [];
+    //     // var inArguments = hasInArguments ? activity.arguments.execute.inArguments : [];
 
-        // console.log('Activity Data:', JSON.stringify(activity, null, 4));
-        // console.log('InArguments:', inArguments);
+    //     // console.log('Activity Data:', JSON.stringify(activity, null, 4));
+    //     // console.log('InArguments:', inArguments);
 
-        // var mobileNumberArg = inArguments.find(arg => arg.mobileNumber);
-        // if (mobileNumberArg) {
-        //     $('#MobileNumber').val(mobileNumberArg.mobileNumber);
-        // }
-    }
+    //     // var mobileNumberArg = inArguments.find(arg => arg.mobileNumber);
+    //     // if (mobileNumberArg) {
+    //     //     $('#MobileNumber').val(mobileNumberArg.mobileNumber);
+    //     // }
+    // }
 
-    function onDoneButtonClick() {
-        activity.metaData.isConfigured = true;
-        var mobileNumber = $('#MobileNumber').val().trim();
-
-        if (!mobileNumber) {
-            alert("⚠ Please enter a mobile number!");
-            return;
-        }
-
-        activity.arguments.execute.inArguments = [{ mobileNumber }];
-        activity.name = `Check Opt-In for ${mobileNumber}`;
-
-        console.log('Updated Activity:', JSON.stringify(activity, null, 4));
-        connection.trigger('updateActivity', activity);
-    }
-
-    function onBackButtonClick() {
-        connection.trigger('prevStep');
-    }
-
-    function onCancelButtonClick() {
-        connection.trigger('setActivityDirtyState', false);
-        connection.trigger('requestInspectorClose');
-    }
+    connection.on("initActivity", function (data) {
+        console.log("Activity Data:", data);
+        $("#mobileNumber").val(data.inArguments?.[0]?.mobileNumber || "");
+    });
+    
 });
