f9ad597fd8d427df944632fbd035914fa225f287
RevanthDigitalInterakt <revanth@rezultize.com>
2025-03-11 17:48:16 +0530

    mobile number removed and load

--- activity/src/index.js
+++ activity/src/index.js
@@ -1,48 +1,18 @@
-// define(["postmonger"], function (Postmonger) {
-//     var connection = new Postmonger.Session();
-//     var activity = {};
-
-//     connection.on("initActivity", function (data) {
-//         activity = data;
-//         console.log("SFMC Activity Initialized", data);
-//         connection.trigger("ready");
-//     });
-
-//     connection.on("requestedSave", function () {
-//         activity.arguments.execute.inArguments = [{ "mobileNumber": "{{Contact.Attribute.MobileNumber}}" }];
-//         connection.trigger("updateActivity", activity);
-//     });
-// });
-
-
 define(["postmonger"], function (Postmonger) {
     var connection = new Postmonger.Session();
     var activity = {};
 
-    // SFMC Journey Builder initializes the activity
     connection.on("initActivity", function (data) {
         activity = data;
         console.log("SFMC Activity Initialized", data);
         connection.trigger("ready");
     });
 
-   
-    document.addEventListener("DOMContentLoaded", function () {
-        document.getElementById("submitBtn").addEventListener("click", function () {
-            var mobileNumber = document.getElementById("mobileNumber").value;
-            console.log("Captured Mobile Number:", mobileNumber);
-
-            // Store mobile number in activity arguments
-            activity.arguments.execute.inArguments = [{ "mobileNumber": mobileNumber }];
-            
-            // Notify SFMC that activity is updated
-            connection.trigger("updateActivity", activity);
-        });
-    });
-
-    // Handle Save Event in SFMC
     connection.on("requestedSave", function () {
+        activity.arguments.execute.inArguments = [{ "mobileNumber": "{{Contact.Attribute.MobileNumber}}" }];
         connection.trigger("updateActivity", activity);
     });
 });
+
+
 
